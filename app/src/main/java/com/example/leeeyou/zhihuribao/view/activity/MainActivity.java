07496b7abc512f92da4eb3c714c095050a7518f8
Leee You <leeeyou@163.com>
2016-05-20 23:23:12 +0800

    commit MainActivity

--- app/src/main/java/com/example/leeeyou/zhihuribao/view/activity/MainActivity.java
+++ app/src/main/java/com/example/leeeyou/zhihuribao/view/activity/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.leeeyou.zhihuribao.view.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 
 import com.example.leeeyou.zhihuribao.R;
+import com.example.leeeyou.zhihuribao.view.MyFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends Base_Original_Activity {
 
@@ -10,6 +15,18 @@ public class MainActivity extends Base_Original_Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add("每日推荐", MyFragment.class)
+                .add("知乎专栏", MyFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
     }
 
     @Override
