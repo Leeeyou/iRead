3f58831b18f1e1f7378d4df05c7d2199d3b6271e
LeeeYou <leeeyou@foxmail.com>
2018-12-13 10:00:32 +0800

    const val

--- app/src/main/java/com/xyz/leeeyou/zhihuribao/vi/one/OneMultiItemEntity.kt
+++ app/src/main/java/com/xyz/leeeyou/zhihuribao/vi/one/OneMultiItemEntity.kt
@@ -7,18 +7,18 @@ import com.xyz.leeeyou.zhihuribao.data.model.one.Weather
 /**
  * ClassName:   OneMultiItemEntity
  * Description: [ One ] module, multi-layout entity class
- * 
- * Author:      leeeyou                             
+ *
+ * Author:      leeeyou
  * Date:        2018/2/24 14:31
  */
 class OneMultiItemEntity(private val itemType: Int,
                          val indexData: OneIndex? = null,
                          val weather: Weather? = null) : MultiItemEntity {
     companion object {
-        val BLANK = -1
-        val WEATHER = 0
-        val TOP = 1
-        val READ = 2
+        const val BLANK = -1
+        const val WEATHER = 0
+        const val TOP = 1
+        const val READ = 2
     }
 
     override fun getItemType(): Int = itemType
