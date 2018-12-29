0036110d40d100afaa8e1331507faf55c8927f27
LeeeYou <leeeyou@foxmail.com>
2018-12-29 18:08:34 +0800

    Adjust the background color of the Fragment

--- app/src/main/java/com/leeeyou/wanandroid/WanAndroidFragment.kt
+++ app/src/main/java/com/leeeyou/wanandroid/WanAndroidFragment.kt
@@ -1,12 +1,14 @@
 package com.leeeyou.wanandroid
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.leeeyou.R
 import com.leeeyou.manager.BaseFragment
-import com.leeeyou.util.ViewPagerAdapter
 import com.leeeyou.util.inflate
 import kotlinx.android.synthetic.main.fragment_wan_android.*
 
@@ -32,9 +34,17 @@ class WanAndroidFragment : BaseFragment() {
         val titleList = arrayOf("推荐", "体系", "项目")
 
         mViewPagerAdapter = ViewPagerAdapter(activity!!.supportFragmentManager, fragmentList, titleList)
-        wanAndroidViewPager.adapter = mViewPagerAdapter
 
+        wanAndroidViewPager.adapter = mViewPagerAdapter
         wanAndroidTabLayout.setupWithViewPager(wanAndroidViewPager)
     }
 
+    private class ViewPagerAdapter(fm: FragmentManager, private val mFragmentList: Array<BaseFragment>, private val titleList: Array<String>)
+        : FragmentStatePagerAdapter(fm) {
+        override fun getItem(position: Int): Fragment = mFragmentList[position]
+
+        override fun getCount(): Int = mFragmentList.size
+
+        override fun getPageTitle(position: Int): CharSequence = titleList[position]
+    }
 }
