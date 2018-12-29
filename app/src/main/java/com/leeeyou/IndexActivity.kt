5b81d2fe7026adc3f257e641ad1486dfc9e82143
LeeeYou <leeeyou@foxmail.com>
2018-12-29 18:20:54 +0800

    Change TabLayout indicator color and height

--- app/src/main/java/com/leeeyou/IndexActivity.kt
+++ app/src/main/java/com/leeeyou/IndexActivity.kt
@@ -3,6 +3,7 @@ package com.leeeyou
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
+import android.view.Window
 import android.widget.ImageView
 import com.leeeyou.wanandroid.WanAndroidFragment
 import com.leeeyou.weather.WeatherFragment
@@ -26,6 +27,7 @@ class IndexActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_index)
         initViewPager()
     }
